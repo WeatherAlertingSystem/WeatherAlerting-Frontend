57f64f3993d57554d22286e3a3353ea92d7672e1
Dawid Karoński <dawid.karonski@orange.com>
2023-03-27 23:57:14 +0200

    Add config containing backend url to assets, fetching that config by http on the go

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import config from '../assets/config.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'WeatherAlerting-Frontend';
+  backendUrl!: string;
+
+  constructor(private http: HttpClient) {
+    this.getAssetConfigJson().subscribe({
+      next: (value: typeof config) => {
+        this.backendUrl = value.backendApiUrl;
+      },
+    });
+  }
+
+  getAssetConfigJson(): Observable<typeof config> {
+    return this.http.get<typeof config>('../assets/config.json');
+  }
 }
